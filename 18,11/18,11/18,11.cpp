df7cda8254d7c79a66f4096a5e5ea32aca223cf3
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-11-17 19:42:16 +0200

    Add files via upload

---
+++ 18,11/18,11/18,11.cpp
@@ -0,0 +1,41 @@
+﻿#include <cstdio>
+#include <cctype>
+
+void encryptFile(const char* inputFile, const char* outputFile, int shift) {
+    FILE* input = fopen(inputFile, "r");
+    FILE* output = fopen(outputFile, "w");
+
+    if (input == nullptr || output == nullptr) {
+        perror("Error opening files");
+        return;
+    }
+
+    int ch;
+    while ((ch = fgetc(input)) != EOF) {
+        if (isalpha(ch)) {
+            char shifted = ch + shift;
+            if ((isupper(ch) && shifted > 'Z') || (islower(ch) && shifted > 'z')) {
+                shifted -= 26; 
+            }
+            fputc(shifted, output);
+        }
+        else {
+            fputc(ch, output); 
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+}
+
+int main() {
+    const char* inputFile = "input.txt"; 
+    const char* outputFile = "output.txt"; 
+    int shift = 3; 
+
+    encryptFile(inputFile, outputFile, shift);
+
+    printf("Encryption complete!\n");
+    return 0;
+}
+
